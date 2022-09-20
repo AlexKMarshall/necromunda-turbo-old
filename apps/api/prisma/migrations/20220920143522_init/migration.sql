60d42da62c51215250a7972487c308432a33609f
AlexKMarshall <alex.k.marshall83@gmail.com>
2022-09-20 16:44:52 +0100

    feat: add prisma

---
+++ apps/api/prisma/migrations/20220920143522_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Faction" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Faction_name_key" ON "Faction"("name");
--- apps/api/src/router.ts
+++ apps/api/src/router.ts
@@ -1,6 +1,25 @@
 import { Router } from 'express'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 export const router: Router = Router()
 
-router.get('/factions', (req, res) => {
-  res.send({ items: [], meta: { pagination: { totalCount: 0 } } })
+router.get('/factions', async (req, res) => {
+  try {
+    const factions = await prisma.faction.findMany()
+    const totalCount = await prisma.faction.count()
+    res.status(200)
+    res.send({ items: factions, meta: { pagination: { totalCount } } })
+  } catch (error) {
+    throw error
+  }
+})
+
+router.post('/factions', async (req, res) => {
+  try {
+    const savedFaction = await prisma.faction.create({ data: req.body })
+    res.status(201)
+    res.send(savedFaction)
+  } catch (error) {
+    throw error
+  }
 })
