c53c006658fb5705c2076569656c21c9cbaf97f4
AlexKMarshall <alex.k.marshall83@gmail.com>
2022-09-21 00:17:17 +0100

    refactor: extract faction controller

---
+++ apps/api/src/controllers/faction.controller.ts
@@ -0,0 +1,24 @@
+import { Context } from 'koa'
+import {
+  createFaction,
+  CreateFactionDTO,
+  getFactionCount,
+  getFactions,
+} from '../faction'
+
+export const getFactionCollection = async (ctx: Context) => {
+  const [factions, totalCount] = await Promise.all([
+    getFactions,
+    getFactionCount,
+  ])
+  ctx.body = {
+    items: factions,
+    meta: { pagination: { totalCount } },
+  }
+}
+
+export const postFaction = async (ctx: Context) => {
+  const createFactionDTO = CreateFactionDTO.parse(ctx.request.body)
+  const savedFaction = await createFaction(createFactionDTO)
+  ctx.body = savedFaction
+}
--- apps/api/src/router.ts
+++ apps/api/src/router.ts
@@ -1,29 +1,14 @@
 import Router from '@koa/router'
 import {
-  createFaction,
-  CreateFactionDTO,
-  getFactionCount,
-  getFactions,
-} from './faction'
+  getFactionCollection,
+  postFaction,
+} from './controllers/faction.controller'
 
 export const router = new Router()
 
 router.get('/', (ctx) => {
   ctx.body = 'Hello world'
 })
-router.get('/factions', async (ctx) => {
-  const [factions, totalCount] = await Promise.all([
-    getFactions,
-    getFactionCount,
-  ])
-  ctx.body = {
-    items: factions,
-    meta: { pagination: { totalCount } },
-  }
-})
-router.post('/factions', async (ctx) => {
-  const createFactionDTO = CreateFactionDTO.parse(ctx.request.body)
-  const savedFaction = await createFaction(createFactionDTO)
-  ctx.body = savedFaction
-})
+router.get('/factions', getFactionCollection)
+router.post('/factions', postFaction)
 router.get('/healthz', (ctx) => (ctx.body = { ok: true }))
