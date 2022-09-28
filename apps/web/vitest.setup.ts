15f9d0b7621e78684682a93f9f4f20f82c7b3651
AlexKMarshall <alex.k.marshall83@gmail.com>
2022-09-28 19:02:53 +0100

    test: add jest dom matchers

--- apps/web/vitest.setup.ts
+++ apps/web/vitest.setup.ts
@@ -1,4 +1,5 @@
-import matchers from '@testing-library/jest-dom/matchers'
-import { expect } from 'vitest'
+import '@testing-library/jest-dom/extend-expect'
+// import matchers from '@testing-library/jest-dom/matchers'
+// import { expect } from 'vitest'
 
-expect.extend(matchers)
+// expect.extend(matchers)
