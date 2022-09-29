a2e27543be652643719df0d8619288d0a2027fe3
AlexKMarshall <alex.k.marshall83@gmail.com>
2022-09-29 01:15:06 +0100

    feat: document high level types for FoundGang

--- apps/domain/src/gang.ts
+++ apps/domain/src/gang.ts
@@ -1,1 +1,35 @@
-export type Gang = {}
+import * as E from 'fp-ts/Either'
+
+export type Gang = {
+  id: GangId
+  factionId: FactionId
+  name: GangName
+}
+
+type FactionId = unknown
+type GangId = unknown
+type GangName = unknown
+
+type UnvalidatedGang = {
+  factionId: string
+  name: string
+}
+
+type FoundGangEvents = {
+  gangAcknowledgementSent: GangAcknowledgementSent
+  foundedGang: FoundedGang
+}
+
+type GangAcknowledgementSent = unknown
+type FoundedGang = unknown
+
+type FoundGangError = ValidationError[]
+
+type ValidationError = {
+  fieldName: string
+  errorDescription: string
+}
+
+type FoundGang = (
+  unvalidatedGang: UnvalidatedGang
+) => E.Either<FoundGangError, FoundGangEvents>
