cfd52f21094f9996e03ce3e9a5ff8dd0d8420e12
AlexKMarshall <alex.k.marshall83@gmail.com>
2022-10-07 19:39:19 +0100

    feat: implement basic faction controller

--- apps/api/src/factionNew.ts
+++ apps/api/src/factionNew.ts
@@ -1,10 +1,13 @@
+import * as IO from 'fp-ts/IO'
+import * as T from 'fp-ts/Task'
 import * as N from '@necromunda/domain'
 import { flow, pipe } from 'fp-ts/lib/function'
-import * as T from 'fp-ts/Task'
 import { prisma } from './prisma'
 import * as TE from 'fp-ts/TaskEither'
 import * as O from 'fp-ts/Option'
+import { Context, Request } from 'koa'
 
+// repository infrastructure
 const checkFactionNameExistsNew = (name: string) =>
   pipe(
     prisma.faction.findFirst({ where: { name } }),
@@ -25,6 +28,32 @@ const checkFactionNameExistsNew = (name: string) =>
   )
 class DBError extends Error {}
 
-const domainCreateFactionTE = N.Faction.createFaction({
+// connect persistance to create domain service with all dependencies attached
+const domainCreateFaction = N.Faction.createFaction({
   checkFactionNameExists: checkFactionNameExistsNew,
 })
+
+type Response = {
+  status: number
+  message: string
+}
+
+const controllerCreateFactionPipeline = flow(
+  domainCreateFaction,
+  TE.fold(
+    () => T.of({ status: 500, message: 'Oh no' }),
+    () => T.of({ status: 200, message: 'Faction created' })
+  )
+)
+
+type Controller = (ctx: Context) => T.Task<void>
+export const createFactionController: Controller = (ctx) => {
+  return pipe(
+    ctx.request.body as any,
+    controllerCreateFactionPipeline,
+    T.map(({ status, message }) => {
+      ctx.status = status
+      ctx.body = { message }
+    })
+  )
+}
