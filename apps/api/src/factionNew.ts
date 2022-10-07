60230d760cf1715536ae78fd67879f87889bb327
AlexKMarshall <alex.k.marshall83@gmail.com>
2022-10-07 12:44:37 +0100

    feat: inject concrete checkFactionNameExists into createFaction

---
+++ apps/api/src/factionNew.ts
@@ -0,0 +1,10 @@
+import * as N from '@necromunda/domain'
+import { pipe } from 'fp-ts/lib/function'
+import * as T from 'fp-ts/Task'
+import { prisma } from './prisma'
+
+const checkFactionNameExists = (name: string) => {
+  return pipe(prisma.faction.findFirst({ where: { name } }), Boolean, T.of)
+}
+
+const domainCreateFaction = N.createFaction({ checkFactionNameExists })
--- apps/domain/src/index.ts
+++ apps/domain/src/index.ts
@@ -1,1 +1,2 @@
 export * from './gang'
+export * from './faction'
