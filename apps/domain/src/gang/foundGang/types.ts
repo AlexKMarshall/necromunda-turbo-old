a97ec0bfb204fb5967d83d9dd7062cd0cb5208e8
AlexKMarshall <alex.k.marshall83@gmail.com>
2022-10-09 16:49:08 +0100

    refactor: change toValidFactionId into task either

--- apps/domain/src/gang/foundGang/types.ts
+++ apps/domain/src/gang/foundGang/types.ts
@@ -3,12 +3,16 @@ import { GangId } from './gangId'
 import { String50 } from '../../common/string50'
 import { GangValidationError } from './implementation'
 import * as E from 'fp-ts/Either'
+import * as TE from 'fp-ts/TaskEither'
 
 /**
  * External dependencies
  */
 
 export type CheckFactionExists = (factionId: FactionId) => boolean
+export type CheckFactionIDExistsTE<NonDomainError = never> = (
+  factionId: FactionId
+) => TE.TaskEither<NonDomainError, boolean>
 
 /**
  * Contains the types of the public API
@@ -33,12 +37,25 @@ export type GangFounded = {
 export type FoundGangEvent = GangFounded
 
 export type FoundGangError = GangValidationError
+export type FoundGangSuccess = {
+  gangFounded: ValidatedGang
+  events: FoundGangEvent[]
+}
 
 export type FoundGangDependencies = {
   checkFactionExists: CheckFactionExists
 }
+
+export type FoundGangDependenciesTE<NonDomainError = never> = {
+  checkFactionIdExists: CheckFactionIDExistsTE
+}
+
 export type FoundGang = (
   dependencies: FoundGangDependencies
 ) => (
   unvalidatedGang: UnvalidatedGang
 ) => E.Either<FoundGangError, FoundGangEvent[]>
+
+export type FoundGangTE<NonDomainError = never> = (
+  unvalidatedGang: UnvalidatedGang
+) => TE.TaskEither<FoundGangError | NonDomainError, FoundGangSuccess>
